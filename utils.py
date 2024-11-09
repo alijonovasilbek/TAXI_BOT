531b80c908b1096ce0204109528e8a2ceb26683a
Asilbek <147981875+alijonovasilbek@users.noreply.github.com>
2024-11-09 10:37:05 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,46 +1,45 @@
 import re
-
 import time
+import os
+
+BASE_DIR = '/home/tuya/Taxi_Bot/'
 
 def is_valid_phone_number(phone_number):
     return re.match(r'^\+?\d{9,15}$', phone_number) is not None
 
 
 def clear_messages_file(file_path="messages.txt"):
+    file_path = os.path.join(BASE_DIR, file_path)
     with open(file_path, "w", encoding="utf-8") as file:
         file.write("")
 
 
-
-
 def save_message_to_file(message, file_path="messages.txt"):
+    file_path = os.path.join(BASE_DIR, file_path)
     timestamp = str(int(time.time() * 1000))
-    message_with_id = f"{timestamp}|{message.replace('\n', ' ').strip()}"
+    message_with_id = f"{timestamp}|{message.replace(chr(10), ' ').strip()}"
     with open(file_path, "a", encoding="utf-8") as file:
         file.write(message_with_id + "\n")
     return timestamp
 
 
-
 def get_messages_from_file(file_path="messages.txt"):
+    file_path = os.path.join(BASE_DIR, file_path)
     with open(file_path, "r", encoding="utf-8") as file:
         return [msg.strip() for msg in file.readlines()]
 
 
 def remove_message_from_file(timestamp: str, file_path: str = "messages.txt"):
-    # Read all messages from the file
+    file_path = os.path.join(BASE_DIR, file_path)
     messages = get_messages_from_file(file_path)
 
-    # Locate the message that starts with the specified timestamp
     matched_message = next((msg for msg in messages if msg.startswith(timestamp)), None)
 
     if matched_message:
         messages.remove(matched_message)
-        print(f"Removed message: {matched_message}")
-
-        # Write remaining messages back to the file
         with open(file_path, "w", encoding="utf-8") as file:
             for msg in messages:
                 file.write(msg + "\n")
     else:
         print(f"Message with timestamp {timestamp} not found in file.")
+
